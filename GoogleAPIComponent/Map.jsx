9aaaf59de71f4f0f03b0f464f3da079ea727ad00
Gurlabe Dhaliwal <GurlabeD@Ramans-MacBook-Air.local>
2018-11-30 23:25:05 -0800

    setting up react redux

--- GoogleAPIComponent/Map.jsx
+++
@@ -1,9 +0,0 @@
-export class Map extends React.Component {
-    render () {
-        return (
-            <div ref='map'>
-                Hold the fuck up...
-            </div>
-        )
-    }
-}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
-
+//connect in redux store allows you to you use redux on that js page  
+import { connect } from 'react-redux';
+//import all actions 
+import * as actions from './actions';
+import '.App.css';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+  state = {
+    name: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.updateName(this.state.name)
+  }
 
-      </div>
-    );
+
+render() {
+  console.log(this.props);
+  return (
+    <div className="App">
+      <form onSubmit={this.onSubmit}>
+        <input type='text' name='name' onChange={(e) => this.props.updateName(e.target.value)} />
+        <button type='submit' onClick={this.onSubmit}>Submit</button>
+      </form>
+    </div>
+  );
+}
+}
+const mapStateToProps = (state) => {
+  return {
+    ...state 
   }
 }
-
-export default App;
+export default connect(mapStateToProps, actions)(App);
